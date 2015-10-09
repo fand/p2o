e497b0e250cff4b846ac65e1236ac7dcaa1e1a04
fand <fand@gmork.in>
2015-10-09 22:06:06 +0900

    ちょっと整形

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ var npm       = require('npm');
 var fs        = promisify('fs');
 var Mustache  = require('mustache');
 
+var readPackageJson = function (jsonPath) {
+  return fs.readFile(jsonPath, 'utf8')
+    .then(function (file) {
+      var pack = JSON.parse(file);
+      return {
+        package   : pack,
+        proDeps   : pack.dependencies || {},
+        devDeps   : pack.devDependencies || {},
+        proRepos  : null,
+        devRepos  : null,
+        opmlTitle : pack.name + ' : Deps updates',
+        opmlOwner : pack.author,
+      };
+    });
+};
+
+var loadNpm = function () {
+  return promisify(npm.load)({});
+};
+
 var depsToRepos = function (deps) {
   return Promise.all(Object.keys(deps).map(function (depName) {
     return promisify(npm.commands.view)([depName, 'repository'], true)
@@ -15,6 +35,40 @@ var depsToRepos = function (deps) {
   }));
 };
 
+var reposToFeed = function (repos) {
+  return repos.filter(function (repo) {
+    if (repo.type !== 'git') { return false; }
+    if (! /github\.com.*\.git$/.test(repo.url)) { return false; }
+    return true;
+  }).map(function (repo) {
+    var m = repo.url.match(/github\.com[\/\:](.*)\/(.*)\.git$/);
+    var userName = m[1];
+    var repoName = m[2];
+    var htmlUrl = 'https://github.com/' + userName + '/' + repoName;
+    var xmlUrl = htmlUrl + '/releases.atom';
+
+    return {
+      title   : repoName,
+      htmlUrl : htmlUrl,
+      xmlUrl  : xmlUrl,
+    };
+  });
+};
+
+var renderOPML = function (data, outputFilePath) {
+  return fs.readFile(__dirname + '/template.xml', 'utf8')
+    .then(function (template) {
+      return Mustache.render(template, data);
+    })
+    .then(function (outputData) {
+      return fs.writeFile(outputFilePath, outputData, 'utf8');
+    });
+};
+
+var writeOPML = function (path, data) {
+  return fs.writeFile(path, data, 'utf8');
+};
+
 /**
  * @param {string} packageFilePath
  * @param {string} outputFilePath
@@ -22,78 +76,39 @@ var depsToRepos = function (deps) {
  * @return {Promise}
  */
 module.exports = function (packageFilePath, outputFilePath, mode) {
-  var pack, proDeps, devDeps, opmlTitle, opmlOwner;
 
-  fs.readFile(packageFilePath, 'utf8')
-    .then(function (file) {
-      pack    = JSON.parse(file);
-      proDeps = pack.dependencies || {};
-      devDeps = pack.devDependencies || {};
-      opmlTitle = pack.name + ' : Deps updates';
-      opmlOwner = pack.author;
+  var pkg;
+
+  readPackageJson(packageFilePath)
+    .then(function (res) {
+      pkg = res;
     })
+    .then(loadNpm)
     .then(function () {
-      return promisify(npm.load)({});
-    })
-    .then(function () {
-      var proRepos = depsToRepos(proDeps);
-      var devRepos = depsToRepos(devDeps);
+      var proRepos = depsToRepos(pkg.proDeps);
+      var devRepos = depsToRepos(pkg.devDeps);
 
       return Promise.all([proRepos, devRepos]).then(function (repos) {
-        return {
-          pro : repos[0],
-          dev : repos[1],
-        };
+        pkg.proRepos = repos[0];
+        pkg.devRepos = repos[1];
       });
     })
-    .then(function (repos) {
+    .then(function () {
       // 引数によってrepoを変える
       if (mode === 'all') {
-        return repos.pro.concat(repos.dev);
+        pkg.repos = pkg.proRepos.concat(pkg.devRepos);
       }
       if (mode === 'pro') {
-        return repos.pro;
+        pkg.repos = pkg.proRepos;
       }
       if (mode === 'all') {
-        return repos.dev;
+        pkg.repos = pkg.devRepos;
       }
-    })
-    .then(function (repos) {
-      var feeds = repos.filter(function (repo) {
-        if (repo.type !== 'git') { return false; }
-        if (! /github\.com.*\.git$/.test(repo.url)) { return false; }
-        return true;
-      }).map(function (repo) {
-        var m = repo.url.match(/github\.com[\/\:](.*)\/(.*)\.git$/);
-        var userName = m[1];
-        var repoName = m[2];
-        var htmlUrl = 'https://github.com/' + userName + '/' + repoName;
-        var xmlUrl = htmlUrl + '/releases.atom';
-        return {
-          title   : repoName,
-          htmlUrl : htmlUrl,
-          xmlUrl  : xmlUrl,
-        };
-      });
-      return feeds;
-    })
-    .then(function (feeds) {
-      var view = {
-        opmlTitle : opmlTitle,
-        opmlOwner : opmlOwner,
-        feeds     : feeds,
-      };
 
-      return fs.readFile(__dirname + '/template.xml', 'utf8')
-        .then(function (template) {
-          return Mustache.render(template, view);
-        })
-        .catch(function (err) {
-          if (err) { throw err; }
-        });
+      pkg.feeds = reposToFeed(pkg.repos);
     })
-    .then(function (output) {
-      return fs.writeFile(outputFilePath, output, 'utf8');
+    .then(function () {
+      return renderOPML(pkg, outputFilePath);
     })
     .catch(function (err) {
       throw err;
